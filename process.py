4b06849d31bd74034d170184ca60908ad95ced37
Frieda Rong <adeirff@gmail.com>
2020-09-23 20:50:27 -0400

    add tasks to GPT-3 process script

--- process.py
+++ process.py
@@ -1,13 +1,36 @@
 from collections import OrderedDict
 import json
+import numpy as np
 import os
-from typing import Any, Dict, List, Tuple, Optional
+import random
+import sys
+from typing import Any, Callable, Dict, List, Tuple, Optional
 from termcolor import colored
 
-import openai  # type: ignore
+from naclo_problems import run_naclo_test_suite
+from tasks import (
+    gen_borple_1, gen_borple_2, gen_borple_3, 
+    test_copycat_remove, 
+    gen_substitute_1, gen_substitute_2, 
+)
+from synthetic_data import (
+    get_vocab, 
+    sample_multilevel_markov_chain, 
+    sample_from_multilevel_markov_chain, 
+    multilevel_markov_chain_sequence_to_str, 
+    sample_hmm, 
+    sample_from_hmm, 
+    hmm_sequence_to_str, 
+)
 
-# https://beta.openai.com/api-ref
-openai.api_key = open('api-key').read().strip()
+try:
+    import openai  # type: ignore
+
+    # https://beta.openai.com/api-ref
+    openai.api_key = open('api-key').read().strip()
+except Exception as e:
+    print(e)
+    print('Falling back to MockGPT3')
 
 DEFAULT_CACHE_PATH = 'cache.jsonl'
 
@@ -39,10 +62,15 @@ def write_cache(cache: Dict, filename: str = DEFAULT_CACHE_PATH):
             print(json.dumps(item), file=f)
     #print(f"Wrote {len(cache)} cache entries")
 
+def set_seed(seed: int = 0):
+    random.seed(seed)
+    np.random.seed(seed)
+    # tf.random.set_seed(seed)
+    # torch.manual_seed(seed)
+
 class GPT3:
-    def __init__(self, cache: Dict, engine: str = "davinci"):
+    def __init__(self, cache: Dict):
         self.cache = cache
-        self.engine = engine
 
     def make_query(self, **kwargs) -> Dict:
         key = get_key(kwargs)
@@ -52,7 +80,7 @@ class GPT3:
             kwargs = dict(kwargs)
             if 'random' in kwargs:
                 del kwargs['random']
-            response = openai.Completion.create(engine=self.engine, **kwargs)
+            response = openai.Completion.create(engine="davinci", **kwargs)
             self.cache[key] = response
         return response
 
@@ -92,9 +120,55 @@ class GPT3:
                 extra = ''
             print(f'[{len(examples)} examples] {x} -> {colored(predicted_y, RESPONSE_COLOR)}{extra}')
 
+class MockGPT3:
+    def __init__(self, cache: Dict):
+        self.cache = cache
+
+    def make_query(self, **kwargs) -> Dict:
+        key = get_key(kwargs)
+        if key in self.cache:
+            response = self.cache[key]
+        else:
+            kwargs = dict(kwargs)
+            if 'random' in kwargs:
+                del kwargs['random']
+            response = {
+                'choices': []  #
+            }
+            self.cache[key] = response
+        return response
+
+    def complete(self, **kwargs):
+        response = self.make_query(**kwargs) 
+        prompt = kwargs['prompt']
+        del kwargs['prompt']
+        print(make_header(kwargs))
+        print(prompt, end='')
+        for choice in response['choices']:
+            print(colored(choice['text'], RESPONSE_COLOR))
+        print('')
+
+    def few_shot(self, examples: List[Tuple[str, str]], x: str, y: Optional[str] = None, prefix: Optional[str] = None, x_label: str = 'Input', y_label: str = 'Output', **kwargs):
+        prompt = f'{x_label}: {x}\n{y_label}:'
+        if len(examples) > 0:
+            prompt = '\n'.join([f'{x_label}: {x}\n{y_label}: {y}\n' for x, y in examples]) + '\n' + prompt
+        if prefix is not None:
+            prompt = prefix + '\n' + prompt
+        if y is not None:
+            prompt += ' ' + y
+        kwargs['prompt'] = prompt
+        kwargs['stop'] = '\n'
+        response = self.make_query(**kwargs)
+        print(prompt)
+
 def main():
-    cache = read_cache()
-    gpt3 = GPT3(cache)
+    if 'openai' in sys.modules:
+        cache = read_cache()
+        gpt3 = GPT3(cache)
+    else:
+        cache_fname = 'cache_mockgpt3.jsonl'
+        cache = read_cache(cache_fname)
+        gpt3 = MockGPT3(cache)
 
     # Generate free-form stuff
     for i in range(10):
@@ -258,6 +332,138 @@ def main():
 
     # Things beyond few-shot learning (soft influence)
 
-    write_cache(cache)
+    # Begin section (frieda) =============================================================================
+
+    # Data cleaning 
+    prefix = 'Reformat each phone number:'
+    train_examples = [
+        ('4164904115', '(416) 490-4115'),
+        ('528 333 2103', '(528) 333-2103'),
+        ('202-318-9100', '(202) 318-9100'),
+        ('(763)-200-3835', '(763) 200-3835'),
+    ]
+    test_examples = [
+        ('3024105332', '(302) 410-5332'),
+        ('610-105-3145', '(610) 105-3145'),
+        ('(191)-423-9875', '(191) 423-9875'),
+    ]
+    for x, y in train_examples + test_examples:
+        gpt3.few_shot(train_examples, x=x, y=y, temperature=0, prefix=prefix, x_label='Original', y_label='Formatted')
+
+    # What does GPT-3 infer? 
+    prompts = [
+        ', '.join(list('aaa')), 
+        ', '.join(list('abab')), 
+        ', '.join(list('aabb')), 
+        ', '.join(list('aabbcc')), 
+        ', '.join(list('aabbaa')), 
+        ', '.join(list('a1b2c3')), 
+        ', '.join(list('a1b2c3d4e5')), 
+        ', '.join(list('z1y2x3')), 
+        ', '.join(list('z1y2x3w4v5')), 
+        ', '.join(list('z1y2x3')), 
+        ', '.join(list('j1k2l3')), 
+        ', '.join(list('j1k2l3m4n5')), 
+        # Do inferred interleaved patterns match interleaved inferred patterns?
+        ', '.join(list('258')), 
+        ## Does GPT-3 model multiple possible completions 1 2 4 7 11 ... [(n)(n+1)/2+1] and 1 2 4 8 ... [2^n]?
+        ', '.join(list('124')), 
+        ', '.join(list('1247')), 
+        ', '.join(list('1248')), 
+        ', '.join(list('a2b5c8')), 
+        ', '.join(list('a1b2c4')), 
+        ', '.join(list('a1b2c4d7')), 
+        ', '.join(list('a1b2c4d8')), 
+        ', '.join(list('que')), 
+        ', '.join(list('ques')), 
+        ', '.join(list('q2u5e8')), 
+        ', '.join(list('q1u2e4')), 
+        ', '.join(list('q1u2e4s7')), 
+        ', '.join(list('q1u2e4s8')), 
+    ]
+    for prompt in prompts:
+        for i in range(5):
+            gpt3.complete(prompt=prompt, temperature=0, random=i, max_tokens=50)
+            gpt3.complete(prompt='Exercise 1. Continue the pattern:\n' + prompt, temperature=0, random=i, max_tokens=50)
+            gpt3.complete(prompt=prompt.replace(',',''), temperature=0, random=i, max_tokens=50)
+            gpt3.complete(prompt='Exercise 1. Continue the pattern:\n' + prompt.replace(',',''), temperature=0, random=i, max_tokens=50)
+
+    # NACLO problems
+    run_naclo_test_suite(gpt3)
+
+    # Synthetic Markov Chains and HMMs
+    for j in range(5):
+        set_seed(j)
+        mc = sample_multilevel_markov_chain(lam=2., alpha=.1)
+        # print(mc)
+        vocab = get_vocab(mc['start_idxs'][-1])
+        # print(len(vocab))
+        # print(vocab)
+        seq_full = []
+        while sum(len(seq) for seq in seq_full) < 512: 
+            seq = sample_from_multilevel_markov_chain(mc)
+            seq_full.append(seq)
+        prompt_full = ' '.join([multilevel_markov_chain_sequence_to_str(mc, seq, vocab, delimiter='') for seq in seq_full])
+        # entropy = compute_entropy_of_markov_chain(mc).  # TODO relate temperature and entropy
+        for n_tokens in [32, 64, 128, 512]:
+            for i in range(5):
+                gpt3.complete(prompt=prompt_full[:n_tokens], temperature=0.7, random=i, max_tokens=1024)
+
+    for j in range(3):
+        set_seed(j)
+        hmm = sample_hmm(alpha_hidden=.1, alpha_visible=.2)
+        # print(hmm)
+        vocab = get_vocab(hmm['start_idxs'][-1])
+        seq = sample_from_hmm(hmm, 512)
+        prompt_full = hmm_sequence_to_str(hmm, seq, vocab, delimiter=' ')
+        for n_tokens in [32, 64, 128, 512]:
+            for i in range(5):
+                gpt3.complete(prompt=prompt_full[:n_tokens], temperature=0.7, random=i, max_tokens=1024)
+
+    # Novel instructions 1: to borple a sequence of letters
+    # Examples:
+    # [
+    #     ('Borple the sequence of letters a b c', 'a b c d')
+    #     ('Borple the sequence of letters a b c thrice', 'a b c d e f')
+    #     ('How many borples are needed to transform a b c into a b c d e f?', '3')
+    # ]
+    set_seed()
+    n_train = 3
+    n_test = 3
+    train_examples = [gen_borple_1() for i in range(n_train)]           \
+                + [gen_borple_2() for i in range(n_train)]              \
+                + [gen_borple_3(possible=True) for i in range(n_train)] \
+                + [gen_borple_3(possible=False) for i in range(n_train)]
+    test_examples = [gen_borple_1() for i in range(n_test)]           \
+                + [gen_borple_2() for i in range(n_test)]              \
+                + [gen_borple_3(possible=True) for i in range(n_test)] \
+                + [gen_borple_3(possible=False) for i in range(n_test)]
+    prefix = 'The following exercises demonstrate what it means to borple a sequence of letters. In words, borpling a sequence of letters modifies the sequence by adding the letter that comes after the last letter of the sequence in the alphabet to the end of the sequence.'
+    for x, y in test_examples:
+        # np.random.permutation(train_examples)
+        gpt3.few_shot(train_examples, x=x, y=y, temperature=0, prefix=prefix, x_label='Exercise', y_label='Answer')
+
+    # Novel instructions 2: letter substitution 
+    ## part i
+    set_seed()
+    prefix = 'The substitute command replaces all occurrences of a given letter with the specified letter. Complete the following exercises by applying the described substitutions.'
+    train_examples = [gen_substitute_1() for i in range(n_train)]
+    test_examples = [gen_substitute_1() for i in range(n_test)]
+    for x, y in test_examples:
+        gpt3.few_shot(train_examples, x=x, y=y, temperature=0, prefix=prefix, x_label='Exercise', y_label='Answer')
+    
+    ## part ii
+    set_seed()
+    prefix = 'The substitute command replaces all occurrences of a given letter with the specified letter. Complete the following exercises by applying the described substitutions.'
+    train_examples = [gen_substitute_2() for i in range(n_train)]
+    test_examples = [gen_substitute_2() for i in range(n_test)]
+    for x, y in test_examples:
+        gpt3.few_shot(train_examples, x=x, y=y, temperature=0, prefix=prefix, x_label='Exercise', y_label='Answer')
+
+    # Novel instructions 3: return of the copycat removal task - TODO make into interaction 
+    set_seed()
+    test_copycat_remove(gpt3)
+
+    write_cache(cache, cache_fname)
 
 main()
